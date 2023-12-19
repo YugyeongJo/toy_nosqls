ee2096bbcdfeb07e3481667c20e695b4bc6361a4
seokcheon <my980519@gmail.com>
2023-12-19 17:51:13 +0900

    add something

--- docs/scoring.py
+++ docs/scoring.py
@@ -42,10 +42,10 @@ pass
 def calculating(list_user_answer,list_quiz):
     sum = 0
     list_sum = []
-    for number in range(len(list_user_answer)) :                              #list_quiz 내용만큼 숫자 받아 for구문 돌리기
+    for number in range(len(list_user_answer)) :  #------------------------------------------------------------------------------------list_quiz 내용만큼 숫자 받아 for구문 돌리기
         for num_question in range(len(list_quiz)) :
             if list_quiz[num_question]["answer"] == list_user_answer[number]["user_answer"][num_question] :
-                sum = sum + list_quiz[number]["score"]      #한사람의 점수 합계구하기
+                sum = sum + list_quiz[number]["score"]  #-------------------------------------------------------------------------------------------------한사람의 점수 합계구하기
                 list_sum.append(sum)
     return list_sum
 def averazing(list_sum,list_user_answer):               
@@ -53,10 +53,10 @@ def averazing(list_sum,list_user_answer):
     for i in range(len(list_sum)):
         total_score = total_score + list_sum[i]
     print(total_score)
-    average = total_score/len(list_user_answer)    # sum 합계를 list_user_answer의 
+    average = total_score/len(list_user_answer)  #--------------------------------------------------------------------------------------total score를 user수 만큼 나눠 평균 구하기
     print("응시자별 채점결과:")
     for number in range(len(list_user_answer)) :
-        print("{}:{}점".format(list_user_answer[number]["user_name"],list_sum[number]))    #list answer에 해당하는 순서의 참여자 이름, 합계
+        print("{}:{}점".format(list_user_answer[number]["user_name"],list_sum[number])) #----------------------------list answer에 해당하는 순서의 참여자 이름과 참여자의 총점 구하기
     print("과목 평균 점수: {}".format(average))
     return  average
 
