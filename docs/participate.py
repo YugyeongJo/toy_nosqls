bffb67bf58e243a22d7889d78cebeee65b8ee907
sjungmin98 <shc980@gmail.com>
2023-12-19 15:36:26 +0900

    hiimjungmin

--- docs/participate.py
+++ docs/participate.py
@@ -56,25 +56,27 @@ list_quiz = [{"question": '파이썬의 기본 자료형이 아닌 것은 무엇
 #         return list_quiz
       
 # list_quiz = find_quiz_list(quiz)
-
-
-while True:
-    list_answer = []
-    str_username = input("응시자의 이름을 입력하세요: ")
-    for i in range(len(list_quiz)):
-        print(list_quiz[i]["question"])
-        print(list_quiz[i]["choice"])
-        user_answer = input("답: ")
-        list_answer.append(user_answer)
-    dic_user = {}
-    dic_user['name'] = str_username
-    dic_user['user_answer'] = list_answer
-
-    quit_input = input("다음 응시자가 있나요? (계속: c, 종료: x): ")
-    if quit_input == 'x':  
-        print("프로그렘이 종료되었습니다.")
-        break   
-    elif quit_input == 'c':
-        pass
-       
+def answermaking():
+    list_user_answer = []
+    while True:
+        list_answer = []
+        str_username = input("응시자의 이름을 입력하세요: ")
+        for i in range(len(list_quiz)):
+            print(list_quiz[i]["question"])
+            print(list_quiz[i]["choice"])
+            user_answer = input("답: ")
+            list_answer.append(user_answer)
+        dic_user = {}
+        dic_user['name'] = str_username
+        dic_user['user_answer'] = list_answer
+        list_user_answer.append(dic_user)
+        quit_input = input("다음 응시자가 있나요? (계속: c, 종료: x): ")
+        if quit_input == 'x':  
+            print("프로그렘이 종료되었습니다.")
+            break   
+        elif quit_input == 'c':
+            pass
+    return list_user_answer
+list = answermaking() 
+print(list)     
     
